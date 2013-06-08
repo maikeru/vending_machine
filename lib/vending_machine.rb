2ad9236bd5a49d1228eb246b5b46bf7f1a22e322
P. Michael Holland <maikeru.horando@gmail.com>
2013-06-08 13:41:58 +0900

    Add multiple coin insertion case.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,5 +1,13 @@
 class VendingMachine
+  def initialize
+    @total_inserted = 0
+  end
+
   def insert_money value
-    value
+    @total_inserted += value
+  end
+
+  def get_total_inserted
+    @total_inserted
   end
 end
--- test/vending_machine_test.rb
+++ test/vending_machine_test.rb
@@ -4,9 +4,19 @@ require_relative '../lib/vending_machine'
 describe VendingMachine do
   [10, 50, 100, 500, 1000].each do |denomination|
     describe "when #{denomination} is inserted" do
-      it 'returns the current total' do
-        VendingMachine.new.insert_money(denomination).must_equal denomination
+      machine = VendingMachine.new
+      it 'returns the value inserted' do
+        machine.insert_money(denomination).must_equal denomination
       end
+    end
+  end
+  describe "when multiple denominations are inserted" do
+    it 'returns the total for all the denominations' do
+      machine = VendingMachine.new
+      [10, 50, 100, 500, 1000].each do |denomination|
+        machine.insert_money(denomination)
+      end
+      machine.get_total_inserted.must_equal 1660
     end
   end
 end
