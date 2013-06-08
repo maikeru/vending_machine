b56f41975e1b28ed36905a97b7130dcc31f3c8d4
P. Michael Holland <maikeru.horando@gmail.com>
2013-06-08 15:19:51 +0900

    Implement step 1 (refunding unsupported denominations)

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,17 +1,23 @@
 class VendingMachine
+  DENOMINATIONS = [10, 50, 100, 500, 1000]
+
   def initialize
     @total_inserted = 0
   end
 
   def insert_money value
-    @total_inserted += value
+    if DENOMINATIONS.include? value
+      @total_inserted += value
+    else
+      refund value
+    end
   end
 
   def get_total_inserted
     @total_inserted
   end
 
-  def refund
-    get_total_inserted
+  def refund value=get_total_inserted
+    "Refund: #{value}"
   end
 end
--- test/vending_machine_test.rb
+++ test/vending_machine_test.rb
@@ -3,26 +3,40 @@ require_relative '../lib/vending_machine'
 DENOMINATIONS = [10, 50, 100, 500, 1000]
 
 describe VendingMachine do
-  DENOMINATIONS.each do |denomination|
-    describe "when #{denomination} is inserted" do
+  describe "when individual coins/notes inserted" do
+    DENOMINATIONS.each do |denomination|
       machine = VendingMachine.new
-      it 'returns the value inserted' do
-        machine.insert_money(denomination).must_equal denomination
+      describe "when #{denomination} is inserted" do
+        it 'returns the value inserted' do
+          machine.insert_money(denomination).must_equal denomination
+        end
       end
     end
   end
+
+  let(:machine) { VendingMachine.new }
   describe "when multiple coins/notes are inserted" do
     it 'returns the total for all the coins/notes' do
-      machine = VendingMachine.new
       insert_all machine, DENOMINATIONS
       machine.get_total_inserted.must_equal 1660
     end
   end
   describe "when a refund is requested" do
     it "returns the total amount of money insterted" do
-      machine = VendingMachine.new
       insert_all machine, DENOMINATIONS
-      machine.refund.must_equal DENOMINATIONS.reduce(:+)
+      machine.refund.must_equal "Refund: #{DENOMINATIONS.reduce(:+)}"
+    end
+  end
+  describe "when invalid money is inserted" do
+    it "doesn't increase the total" do
+      machine.insert_money 10
+      machine.insert_money 5
+      machine.get_total_inserted.must_equal 10
+    end
+
+    it "refunds the money" do
+      machine.insert_money 10
+      machine.insert_money(5).must_equal "Refund: 5"
     end
   end
 
